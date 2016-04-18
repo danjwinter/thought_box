fe30c03a265760c3d2842a67ec062d36ac32ce95
Dan Winter <dan.j.winter@gmail.com>
2016-04-18 17:40:07 -0600

    add tests for read filters

---
+++ spec/feature/user_can_filter_by_read_and_unread_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.feature "User views links" do
+  scenario "and can filter by read or unread" do
+    i_need_javascript do
+      user = create(:user_with_links)
+      google = user.links.create(url: 'http://google.com', title: 'google', read: true)
+      yahoo = user.links.create(url: 'http://yahoo.com', title: 'yahoo')
+
+      visit root_path
+
+      fill_in "Email", with: "Gob@gmail.com"
+      fill_in "Password", with: "password"
+      click_on "Log in"
+
+      page.find('.filter-by-read').click
+      # binding.pry
+      expect(page).to have_selector("#link-#{google.id}", visible: true)
+      expect(page).to have_selector("#link-#{yahoo.id}", visible: false)
+
+
+      page.find('.filter-by-unread').click
+      expect(page).to have_selector("#link-#{google.id}", visible: false)
+      expect(page).to have_selector("#link-#{yahoo.id}", visible: true)
+
+      page.find('.show-all').click
+      expect(page).to have_selector("#link-#{google.id}", visible: false)
+      expect(page).to have_selector("#link-#{yahoo.id}", visible: true)
+    end
+  end
+end
